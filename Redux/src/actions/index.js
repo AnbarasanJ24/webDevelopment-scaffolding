49801ea76c6986d3928d4c2c59ff6882986827d2
Anbarasan <anbarasanj24@gmail.com>
2021-09-20 08:12:52 +0530

    Added Redux thunk syntax

--- Redux/src/actions/index.js
+++ Redux/src/actions/index.js
@@ -8,3 +8,34 @@ export const actionCreatorName = (payload) => {
         payload: payload
     }
 }
+
+// Action creator is responsible for Making API call
+// If action creator return function then same will be handled by redxu thunk 
+
+export const apiCall = () => async dispatch => {
+    const response = await jsonPlaceHolder.get('/posts');
+    dispatch({
+        type: 'ACTION NAME',
+        payload: response
+    })
+}
+
+export const apiCall = () => {
+
+    return async function (dispatch, getState) {
+        const response = await jsonPlaceHolder.get('/posts');
+        dispatch({
+            type: 'ACTION NAME',
+            payload: response
+        })
+    }
+}
+
+// Example
+export const fetchPost = () => async dispatch => {
+    const response = await jsonPlaceHolder.get('/posts');
+    dispatch({
+        type: 'FETCH POSTS',
+        payload: response
+    })
+}
